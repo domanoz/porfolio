a489827a2d69280901fba4ef9a502005e503a627
domanoz <mikolaj-domanski@wp.pl>
2020-08-11 00:11:40 +0200

    Hero added

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import heroimg from "../img/hero-img-main.png";
-
+import SocialLinks from "../constants/socialLinks";
 import "../css/Hero.css";
 
 function Hero() {
   return (
     <header className="hero">
       <div className="section-center hero-center">
-        <article className="hero-info"></article>
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>i'm mikołaj</h1>
+            <h4>fullstack javascript developer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
 
         <img className="hero-img" src={heroimg} alt="" />
       </div>
