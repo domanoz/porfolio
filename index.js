5d51fe0fef102b1ec1ff49b0d05c35dc3a73fc01
domanoz <mikolaj-domanski@wp.pl>
2020-09-14 21:47:10 +0200

    Trying new solutions

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const server = require("./server");
+
 const port = process.env.PORT || 4000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 server.listen(port, () => {
   console.log(`Listening at port ${port}`);
