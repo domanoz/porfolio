4730f6c5a25cdab6fc5b28537ee519f560b286eb
domanoz <mikolaj-domanski@wp.pl>
2020-09-14 21:05:59 +0200

    fix v6

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-require("dotenv").config();
-
 const server = require("./server");
-const port = require("./config").port;
+const { port } = require("./config");
 
 server.listen(port, () => {
   console.log(`Listening at port ${port}`);
