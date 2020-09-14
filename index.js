b9ce8a3d01ca8dfd0e9b29a711ee94f50079ac95
domanoz <mikolaj-domanski@wp.pl>
2020-09-14 22:26:50 +0200

    Changing routes 2

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const server = require("./server");
 const path = require("path");
 const port = process.env.PORT || 4000;
 
-const publicPath = path.join(__dirname, "..", "public");
-server.use(express.static(publicPath));
-
 server.listen(port, () => {
   console.log(`Listening at port ${port}`);
 });
