c6f32ce1d3acd6b2de7c6e6c39819bf5c3a2168c
domanoz <mikolaj-domanski@wp.pl>
2020-09-14 23:33:42 +0200

    Again fix

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const server = require("./server");
-const path = require("path");
+const express = require("express");
 const port = process.env.PORT || 4000;
+
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static("client/build"));
+
+  const path = require("path");
+  server.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 server.listen(port, () => {
   console.log(`Listening at port ${port}`);
