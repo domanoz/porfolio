7b8e495d44991aa7c333ded0789d6e16b07251c5
domanoz <mikolaj-domanski@wp.pl>
2020-09-14 21:11:29 +0200

    Fix v8

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const server = require("./server");
 const port = process.env.PORT || 4000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 server.listen(port, () => {
   console.log(`Listening at port ${port}`);
 });
